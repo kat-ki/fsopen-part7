fa2d6d10fecb5fb31c02ce4e0b0cf8163a8a4f3e
kat-ki <glraskat@gmail.com>
2024-09-14 20:40:00 +0200

    fix: refactored AC into thunks, added types

--- bloglist-frontend-redux/src/components/Blog.jsx
+++ bloglist-frontend-redux/src/components/Blog.jsx
@@ -1,5 +1,5 @@
 import {useDispatch, useSelector} from "react-redux";
-import {removeBlog, selectAllBlogs, selectSingleBlog, updateBlogLikes} from "../state/reducers/blogsSlice.js";
+import {removeBlog, selectSingleBlog, updateBlogLikes} from "../state/reducers/blogsSlice.js";
 import {useNavigate, useParams} from "react-router-dom";
 import {setNotification} from "../state/reducers/notificationSlice.js";
 import {setStatus} from "../state/reducers/statusSlice.js";
--- bloglist-frontend-redux/src/state/reducers/usersSlice.js
+++ bloglist-frontend-redux/src/state/reducers/usersSlice.js
@@ -1,6 +1,5 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import userService from '../../services/users.js'
-import {fetchAllBlogs} from "./blogsSlice.js";
 
 const initialState = [];
 
